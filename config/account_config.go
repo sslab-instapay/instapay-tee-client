68c63fc46c00c7c715774d406044a9b5c8af8a1c
ChaeByunghoon <cbh1203@naver.com>
2019-11-07 16:24:17 +0900

    개인 공개키 추가

--- config/account_config.go
+++ config/account_config.go
@@ -24,16 +24,40 @@ func GetAccountConfig() model.Account {
 			PublicKeyAddress: "0x78902c58006916201F65f52f7834e467877f0500",
 			PrivateKey: "3038465f2b9be0048caa9f33e25b5dc50252f04c078aaddfbea74f26cdeb9f3c",
 		}
-		//TODO 개인키 공개키 발급 new
 	}else if port == 3004{
 		return model.Account{
-			PublicKeyAddress: "0x78902c58006916201F65f52f7834e467877f0500",
-			PrivateKey: "3038465f2b9be0048caa9f33e25b5dc50252f04c078aaddfbea74f26cdeb9f3c",
+			PublicKeyAddress: "0x0ED5B1a31b7D7CA0663f5298461FD44249475dde",
+			PrivateKey: "8a0d44f5799a7af5bb375f5db8ba0d803775784b97fe542f1e7890bb9db881b7",
 		}
 	}else if port == 3005{
 		return model.Account{
-			PublicKeyAddress: "0x78902c58006916201F65f52f7834e467877f0500",
-			PrivateKey: "3038465f2b9be0048caa9f33e25b5dc50252f04c078aaddfbea74f26cdeb9f3c",
+			PublicKeyAddress: "0xbDb2e1dfd4493C966F8c0289502E1674825B7D14",
+			PrivateKey: "bdc7177b984a12e69fa25a478d3b771371986e1792f3fa662f095545513913f1",
+		}
+	}else if port == 3006{
+		return model.Account{
+			PublicKeyAddress: "0x15a3E2348bd529c6dF76C5f840714D1f05Ab6954",
+			PrivateKey: "89bfcbdd969f87ed906a4417ec2b75d8a03d7a0bd720ec291991f1c308c70136",
+		}
+	}else if port == 3007{
+		return model.Account{
+			PublicKeyAddress: "0x2cc07D9903D3F15F5F8C343B34BB41EEB16c90FD",
+			PrivateKey: "1e0487f9f86f2befdd5fd937be7f5d0c719c345b3f2e3c5b5bde2affe76f1c5d",
+		}
+	}else if port == 3008{
+		return model.Account{
+			PublicKeyAddress: "0x421d8797AE9d9AF38Aa7804ecCd14E986beDFb51",
+			PrivateKey: "9138d38da7c99ec4478dc40be20e8fa060bae6cb640fd8bf7059605565213684",
+		}
+	}else if port == 3009{
+		return model.Account{
+			PublicKeyAddress: "0xCe61Cf7937B3f2EC01400C24ef6888F969685F24",
+			PrivateKey: "1c58b2a767a1f3aeac81234009f5ad02e3d81d1b661610450b9ce9edd1f316dd",
+		}
+	}else if port == 3010{
+		return model.Account{
+			PublicKeyAddress: "0xb3c8B15925CE8E6fE018b68d18D69C59D33f5a2e",
+			PrivateKey: "2c69eb06706903ac0698d8e3e7b4ed5115c155a4bcad5902c4e46004f973ddfa",
 		}
 	}
 	return model.Account{
