766e527775e5fa8ce6f87ecad750876e78fecd07
ChaeByunghoon <cbh1203@naver.com>
2020-01-02 20:15:30 +0900

    테스트 코드 추가

---
+++ ethereum_test/deploy_contract.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+	store "github.com/sslab-instapay/instapay-tee-client/contract"
+	"math/big"
+	"fmt"
+	"github.com/ethereum/go-ethereum/crypto"
+	"crypto/ecdsa"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"context"
+)
+
+func main(){
+	client, err := ethclient.Dial("ws://141.223.121.139:8881")
+	if err != nil {
+		log.Fatal(err)
+	}
+	privateKey, err := crypto.HexToECDSA("e113ff405699b7779fbe278ee237f2988b1e6769d586d8803860d49f28359fbd")
+	if err != nil {
+		log.Fatal(err)
+	}
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0) // in wei
+	auth.GasLimit = uint64(300000) // in units
+	auth.GasPrice = gasPrice
+
+	contract, _ :=store.NewContract(fromAddress, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(contract)
+}
---
+++ ethereum_test/read_contract.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+	"github.com/ethereum/go-ethereum/common"
+	store "github.com/sslab-instapay/instapay-go-client/contract"
+	"math/big"
+	"fmt"
+)
+
+func main(){
+	client, err := ethclient.Dial("ws://141.223.121.139:8881")
+	if err != nil {
+		log.Fatal(err)
+	}
+	address := common.HexToAddress("0x092d70BB5c1954F5Fa3EBbb282d0416a5e46c818")
+	instance, err := store.NewContract(address, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	channels, err := instance.Channels(nil, big.NewInt(20))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(channels)
+}
