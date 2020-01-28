65044b1c3c388808c682dec22a7dfd4289821bbb
dongmin <cbh1203@naver.com>
2020-01-29 01:33:16 +0900

    포트에 따라서 계정을 로드하지 않고, 실제로 로드한 계정정보를 반환하도록 수정

--- config/account_config.go
+++ config/account_config.go
@@ -14,11 +14,12 @@ func SetAccountConfig(publicKey string) model.Account{
 }
 
 func GetAccountConfig() model.Account {
-	port, _ := strconv.Atoi(os.Getenv("port"))
-	if port == 3001{
-		return model.Account{
-			PublicKeyAddress: "0xD03A2CC08755eC7D75887f0997195654b928893e",
-		}
-	}
+	// TODO: 이제 포트별 주소지정 필요 없이, SetAccountConfig에서 설정한 계정 정보 불러오기
+	// port, _ := strconv.Atoi(os.Getenv("port"))
+	// if port == 3001{
+	// 	return model.Account{
+	// 		PublicKeyAddress: "0xD03A2CC08755eC7D75887f0997195654b928893e",
+	// 	}
+	// }
 	return personalAccount
 }
