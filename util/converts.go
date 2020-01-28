1bfa16c08fcb91371a18a5e75f93d9c48e3834d9
ChaeByunghoon <cbh1203@naver.com>
2020-01-28 21:12:11 +0700

    유틸 쪽 에러 수정

--- util/converts.go
+++ util/converts.go
@@ -1,5 +1,13 @@
 package util
+
+/*
+#cgo CPPFLAGS: -I/home/xiaofo/sgxsdk/include -I/home/xiaofo/instapay/src/github.com/sslab-instapay/instapay-tee-client
+#cgo LDFLAGS: -L/home/xiaofo/instapay/src/github.com/sslab-instapay/instapay-tee-client -ltee
+
+#include "app.h"
+*/
 import "C"
+
 import (
 	"unsafe"
 	"reflect"
